269ae9c545855650cf65a805e1442cf2a4ab8601
Stefan Hoffmann <stho32github@gmail.com>
2022-07-27 10:50:34 -0700

    [Kanban 30] Split DatabaseConnection and ViewPerformanceTestResult into own files

--- Source/optimalDb/optimalDb.BL/DatabaseConnection.cs
+++ Source/optimalDb/optimalDb.BL/DatabaseConnection.cs
@@ -12,17 +12,3 @@ public class DatabaseConnection : IDatabaseConnection
         ConnectionString = connectionString;
     }
 }
-
-
-public class ViewPerformanceTestResult : IViewPerformanceTestResult
-{
-    public string ViewName { get; }
-
-    public decimal DurationInSeconds { get; }
-
-    public ViewPerformanceTestResult(string viewName, decimal durationInSeconds)
-    {
-        ViewName = viewName;
-        DurationInSeconds = durationInSeconds;
-    }
-}
---
+++ Source/optimalDb/optimalDb.BL/ViewPerformanceTestResult.cs
@@ -0,0 +1,16 @@
+﻿using optimalDb.Interfaces;
+
+namespace optimalDb.BL;
+
+public class ViewPerformanceTestResult : IViewPerformanceTestResult
+{
+    public string ViewName { get; }
+
+    public decimal DurationInSeconds { get; }
+
+    public ViewPerformanceTestResult(string viewName, decimal durationInSeconds)
+    {
+        ViewName = viewName;
+        DurationInSeconds = durationInSeconds;
+    }
+}
