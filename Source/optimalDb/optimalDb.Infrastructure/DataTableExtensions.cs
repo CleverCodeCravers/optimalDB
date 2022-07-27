49661c81154898669af2207881f7235987cbf169
Stefan Hoffmann <stho32github@gmail.com>
2022-07-27 11:06:54 -0700

    [Kanban 30] add DataTableExtensions.cs to simplify the creation of instances from datatables

---
+++ Source/optimalDb/optimalDb.Infrastructure/DataTableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace optimalDb.Infrastructure
+{
+    public static class DataTableExtensions
+    {
+        public static T[] ToInstancesOf<T>(this DataTable dataTable, Func<DataRow, T> converter)
+        {
+            var result = new List<T>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                result.Add(converter(row));
+            }
+
+            return result.ToArray();
+        }
+    }
+}
